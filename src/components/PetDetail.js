be2610487898f0929a784d8cc963f03b7293a08a
guga <gugakhachvani@gmail.com>
2024-02-14 17:52:50 +0400

    modals

--- src/components/PetDetail.js
+++ src/components/PetDetail.js
@@ -1,28 +1,40 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const PetDetail = ({ pet, onRemovePet }) => {
+  const navigate = useNavigate();
+
+  const handleNavigateToTracker = () => {
+    navigate(`/tracker/${pet.id}`);
+  };
+
   return (
-    <div className="bg-gray-100 mb-4 rounded p-4 shadow-lg">
-      <h5 className="text-lg font-bold">{pet.name}</h5>
-      <p>Breed: {pet.breed}</p>
-      <p>Birth Year: {pet.birth_year}</p>
+    <div className="bg-gray-100 mb-4 rounded p-4 shadow-lg flex justify-between items-center">
+      <div>
+        <h5 className="text-lg font-bold">{pet.name}</h5>
+        <p>Breed: {pet.breed}</p>
+        <p>Birth Year: {pet.birth_year}</p>
+      </div>
       {pet.qr_code_data_url && (
         <div className="my-4">
           <img src={pet.qr_code_data_url} alt="QR Code" style={{ maxWidth: '100px', maxHeight: '100px' }} />
         </div>
       )}
-      <div className="mt-4">
-        <Link 
-          to={`/edit-pet/${pet.id}`} 
-          state={{ pet }}
-          className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-1 px-2 rounded mr-2">
-            Edit
-        </Link>
+      <div className="flex items-center">
         <button 
           onClick={() => onRemovePet(pet.id)} 
-          className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded">
+          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded min-w-[120px]">
             Delete
+        </button>
+        <button 
+          className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded min-w-[120px] ml-2"
+          onClick={() => navigate(`/edit-pet/${pet.id}`)}>
+            Edit
+        </button>
+        <button 
+          onClick={handleNavigateToTracker}
+          className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded min-w-[120px] ml-2">
+            Location
         </button>
       </div>
     </div>
--- src/components/Tracker.js
+++ src/components/Tracker.js
@@ -1,18 +1,48 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useParams } from 'react-router-dom';
-import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, useMap } from 'react-leaflet';
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import { fetchPetLocations } from '../Api';
+import Modal from './Modal';
+import markerIconUrl from '../icons/pngwing.com.png';
+
+const customMarkerIcon = new L.Icon({
+    iconUrl: markerIconUrl,
+    iconSize: [35, 46], // size of the icon
+    iconAnchor: [17, 46], // point of the icon which will correspond to marker's location
+});
+
+function SetViewOnClick({ coords }) {
+  const map = useMap();
+  map.setView(coords, map.getZoom());
+
+  return null;
+}
 
 const Tracker = () => {
     const { id } = useParams();
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [selectedLocation, setSelectedLocation] = useState(null);
     const [locations, setLocations] = useState([]);
+    const mapRef = useRef();
+
+    const handleMarkerClick = (location) => {
+        console.log(location)
+        setSelectedLocation(location);
+        setIsModalOpen(true);
+    };
 
     useEffect(() => {
         const fetchLocations = async () => {
             try {
                 const data = await fetchPetLocations(id);
-                setLocations(data.locations); 
+                setLocations(data);
+                if (data.length > 0) {
+                  mapRef.current.flyTo([data[0].latitude, data[0].longitude], 13, {
+                    animate: true,
+                  });
+                }
             } catch (error) {
                 console.error("Failed to load locations:", error);
             }
@@ -21,15 +51,25 @@ const Tracker = () => {
         fetchLocations();
     }, [id]);
 
-    return (
-        <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: "100vh", width: "100%" }}>
-            <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            {locations.map(location => (
-                <Marker key={location.id} position={[location.latitude, location.longitude]} />
-            ))}
-        </MapContainer>
+   return (
+        <>
+            <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: "100vh", width: "100%" }} ref={mapRef}>
+                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                {locations.map((location, index) => (
+                    <Marker key={index} position={[location.latitude, location.longitude]} icon={customMarkerIcon}
+                        eventHandlers={{
+                            click: () => handleMarkerClick(location),
+                        }}
+                    />
+                ))}
+                {/* ... other code */}
+            </MapContainer>
+            {selectedLocation && (
+                <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+                    <div>Date: {selectedLocation.created_at}</div> {/* Render additional location details as needed */}
+                </Modal>
+            )}
+        </>
     );
 };
 
