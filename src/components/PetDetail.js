0494a14bc4436d0180ff7338e660855508f3ed85
guga <gugakhachvani@gmail.com>
2024-02-14 19:12:55 +0400

    pet etails

--- src/components/PetDetail.js
+++ src/components/PetDetail.js
@@ -10,33 +10,39 @@ const PetDetail = ({ pet, onRemovePet }) => {
 
   return (
     <div className="bg-gray-100 mb-4 rounded p-4 shadow-lg flex justify-between items-center">
+    <div className="flex justify-between items-center">
       <div>
         <h5 className="text-lg font-bold">{pet.name}</h5>
         <p>Breed: {pet.breed}</p>
         <p>Birth Year: {pet.birth_year}</p>
       </div>
+    </div>
+    <div className="flex justify-between items-center">
       {pet.qr_code_data_url && (
         <div className="my-4">
           <img src={pet.qr_code_data_url} alt="QR Code" style={{ maxWidth: '100px', maxHeight: '100px' }} />
         </div>
       )}
-      <div className="flex items-center">
+    </div>
+    <div className="flex flex-col sm:flex-row sm:justify-between">
+      <div className="flex flex-col ">
         <button 
           onClick={() => onRemovePet(pet.id)} 
-          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded min-w-[120px]">
+          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded w-[100px] mb-2 sm:mb-0 sm:mr-2">
             Delete
         </button>
         <button 
-          className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded min-w-[120px] ml-2"
+          className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded w-[100px] mb-2 sm:mb-0 sm:mr-2"
           onClick={() => navigate(`/edit-pet/${pet.id}`)}>
             Edit
         </button>
         <button 
           onClick={handleNavigateToTracker}
-          className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded min-w-[120px] ml-2">
+          className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded w-[100px]">
             Location
         </button>
       </div>
+    </div>
     </div>
   );
 };
